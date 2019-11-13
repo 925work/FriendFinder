a7a0307762bcbdfa2352c94ef2959190a95c4f28
Alex Pham <925workk@gmail.com>
2019-11-13 14:38:00 -0700

    completed server.js and package-lock.json.

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+  });
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
