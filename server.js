7761bbcb24233a3b48ca56ae32dfdee9701b95a3
Alex Pham <925workk@gmail.com>
2019-11-13 17:29:23 -0700

    preparing files for heroku testing.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
 
@@ -7,8 +8,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+//require("./app/routing/apiRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
