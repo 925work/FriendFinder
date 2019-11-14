2794f0824cb248b5e4fb86801bfa0cafbdb64db0
Alex Pham <925workk@gmail.com>
2019-11-13 17:14:18 -0700

    added html pages with survey and questions.

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendsArray = [{
+    name: "Paul Blart",
+    photo: "",
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+}, {
+    name: "Bruce Almighty",
+    photo: "",
+    scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+}, {
+    name: "King Leonidas",
+    photo: "",
+    scores: ["4", "4", "4", "4", "4", "4", "4", "4", "4", "4"]
+}, {
+    name: "Sylvanas Windrunner",
+    photo: "",
+    scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+}];
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var path = require("path");
 
 var app = express();
 
@@ -10,14 +9,6 @@ app.use(express.json());
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-  
-app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
-  });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
