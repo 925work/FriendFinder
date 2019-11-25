e924c4a648e0c6503ee7fb129bf3f115b13c8203
Alex Pham <925workk@gmail.com>
2019-11-25 15:28:04 -0700

    fixed errors and users can now match with someone.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,24 @@
-var friendsArray = [{
+var friends = [
+    {
     name: "Paul Blart",
-    photo: "",
+    photo: "https://i2.wp.com/www.audienceseverywhere.net/wp-content/uploads/2015/04/Paul-blart-2.png?fit=470%2C447",
     scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
-}, {
+    }, 
+    {
     name: "Bruce Almighty",
-    photo: "",
+    photo: "https://images.ctfassets.net/7h71s48744nc/28837tV3kkYqkQGcgMwcCG/10573ee869616565294a08cc23403c98/bruce-almighty.jpg",
     scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
-}, {
+    },
+    {
     name: "King Leonidas",
-    photo: "",
+    photo: "https://pbs.twimg.com/profile_images/599698770385719297/EZARvtxK_400x400.jpg",
     scores: ["4", "4", "4", "4", "4", "4", "4", "4", "4", "4"]
-}, {
+    }, 
+    {
     name: "Sylvanas Windrunner",
-    photo: "",
+    photo: "https://vignette.wikia.nocookie.net/moon-guard/images/0/0e/Sylvanas_by_Laura_Sava.jpg/revision/latest/scale-to-width-down/300?cb=20181012202638",
     scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
-}];
+    }
+];
 
-module.exports = friendsArray;
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,43 @@
+var friendsData = require("../data/friends.js");
+
+module.exports = function (app) {
+
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
+
+
+    app.post("/api/friends", function (req, res) {
+
+        var match = {
+            name: "",
+            photo: "",
+            difference: 1000
+        };
+        var userScore = req.body.scores;
+        var totalDifference = 0;
+
+        for (var i = 0; i < friendsData.length; i++) {
+            totalDifference = 0;
+
+            for (var j = 0; j < friendsData[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(userScore[j] - friendsData[i].scores[j]));
+
+                if (totalDifference <= match.difference) {
+
+                    match.name = friendsData[i].name;
+                    match.photo = friendsData[i].photo;
+                    match.difference = totalDifference;
+
+                }
+            }
+        }
+    
+        friendsData.push(req.body);
+        res.json(match);
+
+
+    });
+
+};
